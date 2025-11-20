c8d269935521762c9a83257e60a5956b93e88abe
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-11-21 03:39:26 +1100

    Change(kitty): theme and (not yet) startup args.

--- home/nix/terminal.nix
+++ home/nix/terminal.nix
@@ -1,11 +1,20 @@
 # Kitty terminal and kitten utilities.
 
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   programs.kitty = lib.mkForce {
     enable = true;
+    package = let
+      kitty = pkgs.kitty;
+    in kitty.overrideAttrs (final: prev: {
+      nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+      postInstall = ''
+        wrapProgram $out/bin/kitty
+      '';
+    });
     settings = {
+      include = "./Adapta Nokto Maia.conf";
       enable_audio_bell = false;
-      background_opacity = "0.9";
+      background_opacity = 0.9;
       dynamic_backgronud_opacity = true;
     };
   };
