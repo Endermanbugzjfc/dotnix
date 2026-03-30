e7f1357026c8aa4801bb5c6110a705e721dae11e
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2026-03-30 16:17:51 +1100

    Add(nix)(nix): flake-init util.

--- home/nix/environments/nix.nix
+++ home/nix/environments/nix.nix
@@ -1,19 +1,58 @@
 { inputs, pkgs, ... }: {
-  home.packages = with pkgs; [
-    nh
-    nurl
-    (writeShellScriptBin "kccnp" ''
+  home.packages = let
+    ps = with pkgs; [
+      nh
+      nurl
+
+      kccnp
+      repl
+      repl-update
+      flake-init
+
+      inputs.nix-update-input.packages.${pkgs.stdenv.hostPlatform.system}.default # Command: update-input.
+    ];
+    kccnp = pkgs.writeShellScriptBin "kccnp" ''
       nix-shell --pure --expr '(import <nixpkgs> {}).mkShellNoCC {}'
-    '')
-    (writeShellScriptBin "repl" ''
+    '';
+    repl = pkgs.writeShellScriptBin "repl" ''
       nix repl --expr 'import <nixpkgs> {}'
-    '')
-    (writeShellScriptBin "repl-update" ''
+    '';
+    repl-update = pkgs.writeShellScriptBin "repl-update" ''
       nix repl --expr 'import (fetchTarball
         "https://github.com/nixos/nixpkgs/tarball/nixpkgs-unstable"
       ) {}'
-    '')
+    '';
+    flake-init = with pkgs; let
+      flake-template-top = writeText "flake.nix.a" ''
+        {
+          inputs = {
+            flake-utils.url = "github:numtide/flake-utils";
+            nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+          };
 
-    inputs.nix-update-input.packages.${pkgs.stdenv.hostPlatform.system}.default # Command: update-input.
-  ];
+          outputs = inputs:
+            inputs.flake-utils.lib.eachDefaultSystem (system:
+              let
+                pkgs = (import (inputs.nixpkgs) { inherit system; });
+              in {
+                devShell = pkgs.mkShellNoCC {
+                  packages = with pkgs; [
+      '';
+      flake-template-bottom = writeText "flake.nix.b" ''
+                  ];
+                };
+              }
+            );
+        }
+      '';
+    in writeShellScriptBin "flake-init" ''
+      cat ${flake-template-top} >> flake.nix
+      echo "            $*" >> flake.nix
+      cat ${flake-template-bottom} >> flake.nix
+
+      git add ./flake.nix
+      echo "use flake" >> .envrc
+      direnv allow
+    '';
+  in ps;
 }
