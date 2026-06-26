7724533b74de231639905ab793d434ad2ffbc660
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2026-06-26 15:22:25 +1000

    feat(nix)(compatibility): disable Docker on boot or on socket

--- hosts/nix/compatibility.nix
+++ hosts/nix/compatibility.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     wine-wayland
     winetricks
   ];
 
   virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = false;
+  systemd.sockets.docker.wantedBy = lib.mkForce [];
 }
