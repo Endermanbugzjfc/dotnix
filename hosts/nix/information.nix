4476e412022383be44bc6ef404b48c098689997f
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2026-07-08 21:12:57 +1000

    chore(nix/information): uppercase info folder in etc

--- hosts/nix/information.nix
+++ hosts/nix/information.nix
@@ -1,11 +1,11 @@
 # https://www.reddit.com/r/NixOS/comments/fsummx/how_to_list_all_installed_packages_on_nixos/
 { lib, config, inputs, ... }: {
-  environment.etc."info/current-system-packages".text =
+  environment.etc."INFO/current-system-packages".text =
     let
     packages = builtins.map (p: "${p.name}") config.environment.systemPackages;
     sortedUnique = builtins.sort builtins.lessThan (lib.lists.unique packages);
     formatted = builtins.concatStringsSep "\n" sortedUnique;
   in formatted;
 
-  environment.etc."info/nixpkgs-path".text = builtins.toString inputs.nixpkgs;
+  environment.etc."INFO/nixpkgs-path".text = builtins.toString inputs.nixpkgs;
 }
