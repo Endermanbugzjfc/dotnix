ad446bc2bdcb6d57fbdbc5141352da1ae984d3ec
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2026-07-15 13:06:39 +1000

    feat(nix/editor): claude code

--- home/nix/editor.nix
+++ home/nix/editor.nix
@@ -1,10 +1,14 @@
 # NvChad
 
-{ config, lib, pkgs, inputs, ... }: {
-  config.nixpkgs.overlays = [ inputs.nixche.overlays.neovim-with-lsps ];
-  config.lib.mkLspShell = opt: lsp: pkgs.mkShellNoCC {
-    packages = [
-      (pkgs.neovim.withLsps lsp)
-    ] ++ (opt.packages or []);
-  };
+{ pkgs, ... }: {
+  home.packages = with pkgs; [
+    claude-code
+  ];
+
+  # config.nixpkgs.overlays = [ inputs.nixche.overlays.neovim-with-lsps ];
+  # config.lib.mkLspShell = opt: lsp: pkgs.mkShellNoCC {
+  #   packages = [
+  #     (pkgs.neovim.withLsps lsp)
+  #   ] ++ (opt.packages or []);
+  # };
 }
